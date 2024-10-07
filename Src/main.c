3d9c7c86a17bf2c3141a4e57a0ae61d1b032631c
xiaominwang <mrxmw@qq.com>
2024-10-07 21:08:05 +0800

    feat: 使用库函数操作调试灯

--- Src/main.c
+++ Src/main.c
@@ -2,11 +2,17 @@
 
 int main(void)
 {
-	RCC->APB2ENR = 0x00000010;
-	GPIOC->CRH = 0x00300000;
-	GPIOC->ODR = 0x00000000;//亮调试灯 00002000 灭调试灯
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	// GPIO_SetBits(GPIOC,GPIO_Pin_13);//灭调试灯
+	GPIO_ResetBits(GPIOC,GPIO_Pin_13);//亮调试灯
+
 	while(1)
 	{
-
+		
 	}
 }
